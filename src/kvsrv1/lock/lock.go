eac54616e947be85ab762dc4f8170a7e636c7dbd
lr580 <2277379149@qq.com>
2025-10-04 23:57:40 +0800

    完成 Lab2

--- src/kvsrv1/lock/lock.go
+++ src/kvsrv1/lock/lock.go
@@ -1,8 +1,14 @@
 package lock
 
 import (
-	"6.5840/kvtest1"
+	"time"
+
+	"6.5840/kvsrv1/rpc"
+	kvtest "6.5840/kvtest1"
 )
+
+const IDEN_LEN = 8 // 标识符长度，不为它的任意 value 表示空锁
+const EMPTY_IDENTIDIER = "0"
 
 type Lock struct {
 	// IKVClerk is a go interface for k/v clerks: the interface hides
@@ -11,6 +17,8 @@ type Lock struct {
 	// MakeLock().
 	ck kvtest.IKVClerk
 	// You may add code here
+	identifier string
+	l          string // lock name
 }
 
 // The tester calls MakeLock() and passes in a k/v clerk; your code can
@@ -19,15 +27,37 @@ type Lock struct {
 // Use l as the key to store the "lock state" (you would have to decide
 // precisely what the lock state is).
 func MakeLock(ck kvtest.IKVClerk, l string) *Lock {
-	lk := &Lock{ck: ck}
+	identifier := kvtest.RandValue(IDEN_LEN)
+	lk := &Lock{ck: ck, identifier: identifier, l: l}
 	// You may add code here
 	return lk
 }
 
 func (lk *Lock) Acquire() {
 	// Your code here
+	lk.LoopSet(lk.identifier)
 }
 
 func (lk *Lock) Release() {
 	// Your code here
+	lk.LoopSet(EMPTY_IDENTIDIER)
+}
+
+func (lk *Lock) LoopSet(value string) {
+	for {
+		identifier, version, _ := lk.ck.Get(lk.l)
+		// if err == rpc.ErrNoKey { // 本来默认也是 0
+		// version = 0 // 感觉不会进入，除非先放再取
+		// } // else if err != rpc.OK { // 错误立即重试
+		//	continue
+		// } 不可能分支 else if
+		if len(identifier) == IDEN_LEN && identifier != lk.identifier { // 不是空，不是自己，那么就忙等
+			time.Sleep(100 * time.Millisecond)
+			continue
+		}
+		err := lk.ck.Put(lk.l, value, version)
+		if err == rpc.OK { // 获取成功
+			break
+		}
+	}
 }
