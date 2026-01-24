f49827a69ec03c780de5ef78e24a85191f9b4708
lr580 <2277379149@qq.com>
2026-01-24 22:27:48 +0800

    完成lab5A任务2

--- src/shardkv1/client.go
+++ src/shardkv1/client.go
@@ -9,11 +9,14 @@ package shardkv
 //
 
 import (
+	"time"
 
 	"6.5840/kvsrv1/rpc"
-	"6.5840/kvtest1"
+	kvtest "6.5840/kvtest1"
+	"6.5840/shardkv1/shardcfg"
 	"6.5840/shardkv1/shardctrler"
-	"6.5840/tester1"
+	"6.5840/shardkv1/shardgrp"
+	tester "6.5840/tester1"
 )
 
 type Clerk struct {
@@ -33,6 +36,27 @@ func MakeClerk(clnt *tester.Clnt, sck *shardctrler.ShardCtrler) kvtest.IKVClerk 
 	return ck
 }
 
+func (ck *Clerk) doOp(key string, value string, version rpc.Tversion, isGet bool) (answer string, ver rpc.Tversion, err rpc.Err) {
+	for {
+		cfg := ck.sck.Query()
+		shard := shardcfg.Key2Shard(key)
+		gid, servers, ok := cfg.GidServers(shard)
+		if !ok || gid == 0 {
+			time.Sleep(100 * time.Millisecond)
+			continue
+		}
+		groupClerk := shardgrp.MakeClerk(ck.clnt, servers)
+		if isGet {
+			answer, ver, err = groupClerk.Get(key)
+		} else {
+			err = groupClerk.Put(key, value, version)
+		}
+		if err == rpc.ErrWrongGroup {
+			continue
+		}
+		return answer, ver, err
+	}
+}
 
 // Get a key from a shardgrp.  You can use shardcfg.Key2Shard(key) to
 // find the shard responsible for the key and ck.sck.Query() to read
@@ -41,11 +65,12 @@ func MakeClerk(clnt *tester.Clnt, sck *shardctrler.ShardCtrler) kvtest.IKVClerk 
 // calling shardgrp.MakeClerk(ck.clnt, servers).
 func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
 	// You will have to modify this function.
-	return "", 0, ""
+	return ck.doOp(key, "", 0, true)
 }
 
 // Put a key to a shard group.
 func (ck *Clerk) Put(key string, value string, version rpc.Tversion) rpc.Err {
 	// You will have to modify this function.
-	return ""
+	_, _, err := ck.doOp(key, value, version, false)
+	return err
 }
--- src/shardkv1/shardgrp/client.go
+++ src/shardkv1/shardgrp/client.go
@@ -1,31 +1,87 @@
 package shardgrp
 
 import (
+	"math/rand"
+	"time"
 
 	"6.5840/kvsrv1/rpc"
 	"6.5840/shardkv1/shardcfg"
-	"6.5840/tester1"
+	tester "6.5840/tester1"
 )
 
 type Clerk struct {
 	clnt    *tester.Clnt
 	servers []string
 	// You will have to modify this struct.
+	leader    int //servers[i], last leader
+	clientId  int64
+	requestId int64
 }
 
 func MakeClerk(clnt *tester.Clnt, servers []string) *Clerk {
-	ck := &Clerk{clnt: clnt, servers: servers}
+	ck := &Clerk{clnt: clnt, servers: servers, leader: 0, clientId: rand.Int63(), requestId: 1}
 	return ck
 }
 
 func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
-	// Your code here
-	return "", 0, ""
+	args := rpc.GetArgs{
+		Key:      key,
+		Identity: rpc.Identity{ClientId: ck.clientId, RequestId: ck.requestId},
+	}
+	ck.requestId++
+	for {
+		for i := 0; i < len(ck.servers); i++ {
+			server := (ck.leader + i) % len(ck.servers)
+			reply := rpc.GetReply{}
+			ok := ck.clnt.Call(ck.servers[server], "KVServer.Get", &args, &reply)
+			if !ok || reply.Err == rpc.ErrWrongLeader {
+				continue
+			}
+			if reply.Err == rpc.OK || reply.Err == rpc.ErrNoKey || reply.Err == rpc.ErrWrongGroup {
+				ck.leader = server
+				return reply.Value, reply.Version, reply.Err
+			}
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
 }
 
 func (ck *Clerk) Put(key string, value string, version rpc.Tversion) rpc.Err {
-	// Your code here
-	return ""
+	args := rpc.PutArgs{
+		Key:      key,
+		Value:    value,
+		Version:  version,
+		Identity: rpc.Identity{ClientId: ck.clientId, RequestId: ck.requestId},
+	}
+	ck.requestId++
+	firstRPC := true
+
+	for {
+		for i := 0; i < len(ck.servers); i++ {
+			server := (ck.leader + i) % len(ck.servers)
+			reply := rpc.PutReply{}
+			ok := ck.clnt.Call(ck.servers[server], "KVServer.Put", &args, &reply)
+
+			if !ok || reply.Err == rpc.ErrWrongLeader {
+				continue
+			}
+
+			first := firstRPC
+			firstRPC = false
+
+			ck.leader = server
+			switch reply.Err {
+			case rpc.OK, rpc.ErrNoKey, rpc.ErrWrongGroup:
+				return reply.Err
+			case rpc.ErrVersion:
+				if first {
+					return rpc.ErrVersion
+				}
+				return rpc.ErrMaybe
+			}
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
 }
 
 func (ck *Clerk) FreezeShard(s shardcfg.Tshid, num shardcfg.Tnum) ([]byte, rpc.Err) {
